57fa10f32a2fa978a53c6f37916b00ff0f380c52
Eduardo <eduardoulisses2006@gmail.com>
2025-05-22 21:11:41 -0300

    projeto feito

---
+++ src/JavaScript/app.js
@@ -0,0 +1,44 @@
+let inputNome = document.getElementById('nome');
+let inputPreco = document.getElementById('preco');
+let inputCategoria = document.getElementById('categoria');
+let btnEnviar = document.getElementById('btnEnviar');
+let produtoInteiro = document.getElementById('produto');
+let mensagem = document.getElementById('mensagem');
+let form = document.getElementById('formulario');
+
+btnEnviar.addEventListener('click', function clicar(event) {
+    event.preventDefault()
+
+    let nome = inputNome.value;
+    let preco = inputPreco.value;
+    let categoria = inputCategoria.value;
+    
+    preco = Math.round(preco * 100) / 100;
+    preco = preco.toFixed(2);
+
+    if (preco < 0) {
+        mensagem.textContent = 'Preço Inválido';
+        return
+    }
+    
+    const boxItem = document.createElement('div')
+    boxItem.classList.add('container__itens_produto')
+    boxItem.innerHTML = `
+    <p> Produto: ${nome} - R$${preco} - Categoria: ${categoria}</p>
+    `
+
+    produtoInteiro.append(boxItem);
+
+    if (categoria === 'Alimentos') {
+        boxItem.style.backgroundColor = "#EDBE00FF"
+    }
+
+    if (categoria === 'Tecnologia') {
+        boxItem.style.fontWeight = 'bold'
+    }
+
+    mensagem.textContent = 'Sucesso!'
+
+    form.reset();
+})
+
